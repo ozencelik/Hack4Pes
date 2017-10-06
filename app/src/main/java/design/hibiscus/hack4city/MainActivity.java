a821bee63ba81c63a08af74f75e9a68c04a03d54
kantaronur <onur.kantar@yahoo.com>
2017-10-07 00:31:36 +0300

    deneme mesajı

---
+++ app/src/main/java/design/hibiscus/hack4city/MainActivity.java
@@ -0,0 +1,26 @@
+package design.hibiscus.hack4city;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button firstpage_button ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstpage_button = (Button)findViewById(R.id.button_loginscreen);
+        firstpage_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, secondPage.class));
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/design/hibiscus/hack4city/secondPage.java
@@ -0,0 +1,13 @@
+package design.hibiscus.hack4city;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+}
